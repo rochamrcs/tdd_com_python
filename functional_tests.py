f4e038ff1134efd0a6b44cc0d5d91df7c1d472b1
rochamrcs <mavinicius95@gmail.com>
2026-02-04 18:59:41 -0300

    (Capítulo 2) Extendendo o teste, usando o Unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,38 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations!" in browser.title
-print(browser.title)
-print("OK")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+
+        # O jovem Marcos ouviu falar de um novo app online de tarefas
+        # Ele foi saber mais acessando a pagina do app
+        self.browser.get("http://localhost:8000")
+
+        # Ele percebe que o titulo da pagina menciona a lista de tarefas
+        self.assertIn("To-Do", self.browser.title)
+
+        # Ele é convidade a inserir um item na lista de forma imediata
+        self.fail("Finish the test!")
+
+        # Calmamente ele digita "Comprar um novo curso" na caixa de texto
+        # (Marcos tem um estranho vicio em comprar cursos)
+
+        # Quanto ele aperta enter, a pagina atualiza, e agora ela lista
+        # "1: Comprar um novo curso sobre desenvolvimento" como um item na lista de tarefas
+
+        # A caixa de texto continua ali, convidando-o a inserir um novo item
+        # Então ele insere um novo item "2: Decidir qual o tema do curso"
+
+        # A pagina é atualizada novamente, a agora exibe ambos os itens na lista dele
+
+        # Satisfeito, ele decide ir tirar um cochilo.
+
+if __name__ == "__main__":
+    unittest.main()
