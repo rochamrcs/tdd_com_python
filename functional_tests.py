121526591a642145f1074557cdcc948a4f7e9bdb
rochamrcs <mavinicius95@gmail.com>
2026-04-15 20:28:03 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,22 +20,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ele percebe que o titulo da pagina menciona a lista de tarefas
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text  
+        self.assertIn("To-Do", header_text)
 
         # Ele é convidade a inserir um item na lista de forma imediata
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")  
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # Calmamente ele digita "Comprar um novo curso" na caixa de texto
         # (Marcos tem um estranho vicio em comprar cursos)
+        inputbox.send_keys("Buy peacock feathers")
 
         # Quanto ele aperta enter, a pagina atualiza, e agora ela lista
         # "1: Comprar um novo curso sobre desenvolvimento" como um item na lista de tarefas
+        inputbox.send_keys(Keys.ENTER)  
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")  
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
 
         # A caixa de texto continua ali, convidando-o a inserir um novo item
         # Então ele insere um novo item "2: Decidir qual o tema do curso"
-
         # A pagina é atualizada novamente, a agora exibe ambos os itens na lista dele
 
         # Satisfeito, ele decide ir tirar um cochilo.
+        self.fail("Finish the test!")
+
 
 if __name__ == "__main__":
     unittest.main()
