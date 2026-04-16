4221284dbea72e240e3626e0ac593df51516fa91
rochamrcs <mavinicius95@gmail.com>
2026-04-15 21:15:19 -0300

    Saving User Input: Testing the Database

--- functional_tests.py
+++ functional_tests.py
@@ -9,8 +9,16 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
+
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def test_can_start_a_todo_list(self):
 
@@ -33,19 +41,23 @@ class NewVisitorTest(unittest.TestCase):
 
         # Quanto ele aperta enter, a pagina atualiza, e agora ela lista
         # "1: Comprar um novo curso sobre desenvolvimento" como um item na lista de tarefas
-        inputbox.send_keys(Keys.ENTER)  
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")  
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # A caixa de texto continua ali, convidando-o a inserir um novo item
         # Então ele insere um novo item "2: Decidir qual o tema do curso"
-        # A pagina é atualizada novamente, a agora exibe ambos os itens na lista dele
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        # A pagina é atualizada novamente, a agora exibe ambos os itens na lista dele
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+  
         # Satisfeito, ele decide ir tirar um cochilo.
-        self.fail("Finish the test!")
+
 
 
 if __name__ == "__main__":
