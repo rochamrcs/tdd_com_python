c28400c40c68b9870b0e42455af9e69b8c05e936
rochamrcs <mavinicius95@gmail.com>
2026-04-23 13:35:00 -0300

    ensure isolation and replace sleeps with explicit waits

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,11 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -15,10 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")  
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return  
+            except (AssertionError, WebDriverException):  
+                if time.time() - start_time > MAX_WAIT:  
+                    raise  
+                time.sleep(0.5)
 
 
     def test_can_start_a_todo_list(self):
@@ -43,19 +53,17 @@ class NewVisitorTest(LiveServerTestCase):
         # Quanto ele aperta enter, a pagina atualiza, e agora ela lista
         # "1: Comprar um novo curso sobre desenvolvimento" como um item na lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # A caixa de texto continua ali, convidando-o a inserir um novo item
         # Então ele insere um novo item "2: Decidir qual o tema do curso"
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A pagina é atualizada novamente, a agora exibe ambos os itens na lista dele
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
   
         # Satisfeito, ele decide ir tirar um cochilo.
 
